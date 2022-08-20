3ef8b0e67eaf5c69754ea656cc905338021bd05c
Amanam Israel <amanamisrael@gmail.com>
2022-08-20 15:33:24 +0100

    feat: added sub heading to breadcrumb when on invoices page

--- src/partials/BreadCrumbs.vue
+++ src/partials/BreadCrumbs.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="breadcrumbs">{{ $route.name.toUpperCase() }}</div>
+  <div class="breadcrumbs">
+    <span class="header">
+      {{ $route.name.toUpperCase() }}
+    </span>
+    <span>
+      {{ $route.name === "invoices" ? "/edit invoice (INV-2022-010)" : "" }}
+    </span>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped lang="scss">
 .breadcrumbs {
-  font-weight: bold;
+  display: flex;
+  color: $gray;
+  font-weight: 500;
+
+  .header {
+    font-weight: bold;
+    color: $dark;
+  }
 }
 </style>
