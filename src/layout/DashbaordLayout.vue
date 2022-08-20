878f48c4afb47c4bc64202d9efcb2746d55461e0
Amanam Israel <amanamisrael@gmail.com>
2022-08-20 14:49:18 +0100

    feat: add breadcrumbs to dashboard layout

--- src/layout/DashbaordLayout.vue
+++ src/layout/DashbaordLayout.vue
@@ -2,27 +2,41 @@
   <q-layout>
     <q-page-container>
       <Navbar />
-      <div class="body-container">
-        <router-view></router-view>
-      </div>
+      <section class="body-container">
+        <BreadCrumbs />
+        <div class="layout">
+          <router-view></router-view>
+        </div>
+      </section>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
 import Navbar from "@/partials/NavBar.vue";
+import BreadCrumbs from "@/partials/BreadCrumbs.vue";
 export default {
   name: "DashboardLayout",
-  components: { Navbar },
+  components: { Navbar, BreadCrumbs },
 };
 </script>
 
 <style scoped lang="scss">
-.body-container {
-  background-color: $white;
-  max-width: 800px;
+section.body-container {
+  max-width: 1000px;
   margin: 0 auto;
   border-radius: 0.5rem;
-  padding: 1rem 2rem;
+  padding: 0 2rem;
+  padding-top: 1rem;
+  margin-top: 1rem;
+
+  div.layout {
+    background-color: $white;
+    min-height: 750px;
+    margin-top: 2rem;
+  }
+}
+q-layout {
+  min-height: auto;
 }
 </style>
---
+++ src/partials/BreadCrumbs.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="breadcrumbs">hello world</div>
+</template>
+<script>
+export default {};
+</script>
+<!-- <style scoped lang="scss">
+</style> -->
