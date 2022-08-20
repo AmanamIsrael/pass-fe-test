53b637dc301f70ebf20b589a7a62572c51271ff3
Amanam Israel <amanamisrael@gmail.com>
2022-08-20 20:16:13 +0100

    feat: add invoice card component

---
+++ src/components/invoices/invoiceCard.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <q-card flat class="text-white">
+    <q-card-section class="container">
+      <section class="row items-center">
+        <div class="column vertical-middle">
+          <p><b>Invoice Number</b></p>
+          <p>INV-2022-010</p>
+          <p>Issued Date: 11 jan 2022</p>
+          <p>Due Date: 18 jan 2022</p>
+        </div>
+        <q-space />
+        <div class="column text-right vertical-middle">
+          <p><b>Billed To</b></p>
+          <p>Zaky Grizzly</p>
+          <p>Monlight Agency LTD</p>
+          <p>New York, USA</p>
+        </div>
+      </section>
+    </q-card-section>
+  </q-card>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
+<style scoped lang="scss">
+.q-card {
+  margin: 1.5rem 0;
+  padding: 1rem;
+  border-radius: 2rem;
+  min-height: max-content;
+  background-color: rgb(77, 75, 166, 0.9);
+}
+</style>
--- src/pages/InvoicesPage.vue
+++ src/pages/InvoicesPage.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <InvoiceInfo />
+    <InvoiceCard />
   </div>
 </template>
 <script>
 import InvoiceInfo from "@/components/invoices/InvoiceInfo.vue";
-export default { components: { InvoiceInfo } };
+import InvoiceCard from "../components/invoices/invoiceCard.vue";
+export default { components: { InvoiceInfo, InvoiceCard } };
 </script>
 <style lang=""></style>
