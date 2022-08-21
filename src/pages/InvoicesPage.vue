4eae90709fc23d903b869268564ec5cf8e5099ed
Amanam Israel <amanamisrael@gmail.com>
2022-08-21 15:08:14 +0100

    feat: create server models, api routes for user and invoice and logged in invoice component

--- src/pages/InvoicesPage.vue
+++ src/pages/InvoicesPage.vue
@@ -6,13 +6,34 @@
     <InvoiceFooter />
   </div>
 </template>
-<script>
+<script setup>
 import InvoiceInfo from "@/components/invoices/InvoiceInfo.vue";
 import InvoiceCard from "../components/invoices/invoiceCard.vue";
 import ItemDetail from "../components/invoices/itemDetail.vue";
 import InvoiceFooter from "@/components/invoices/invoiceFooter.vue";
-export default {
-  components: { InvoiceInfo, InvoiceCard, ItemDetail, InvoiceFooter },
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  try {
+    const response = await fetch("api/users");
+    response.json().then((data) => {
+      console.log(data);
+      getInvoices();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const getInvoices = async () => {
+  try {
+    const response = await fetch("api/invoice");
+    response.json().then((data) => {
+      console.log(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 </script>
 <style lang=""></style>
--- src/server/server.js
+++ src/server/server.js
@@ -4,22 +4,48 @@ export const makeServer = ({ environment = "development" } = {}) => {
   let server = createServer({
     environment,
     models: {
-      item: Model,
+      invoice: Model,
       user: Model,
     },
 
     seeds(server) {
       server.create("user", {
         name: "Amanam Israel",
-        items: [],
+        email: "hello@amanamisrael.com",
+        address: {
+          street: "jen Boulevard Street 101",
+          city: "Malang City, 65115",
+          country: "East Java, Indonesia",
+        },
+        payment: {
+          accountName: "Barly Vallendito",
+          accountNumber: "9700 0023 4200 2900",
+          routingNumber: "084009519",
+        },
+      });
+      server.create("invoice", {
+        number: "INV-001",
+        issuedDate: "2020-01-01",
+        dueDate: "2020-01-08",
+        billedTo: {
+          name: "Zaky Grizzly",
+          address: {
+            street: "Monlight Agency LTD",
+            city: "New York, USA",
+          },
+        },
       });
     },
 
     routes() {
       this.namespace = "api";
 
-      this.get("/items", (schema) => {
-        return schema.items.all();
+      this.get("/users", (schema) => {
+        return schema.users.all();
+      });
+
+      this.get("/invoice", (schema) => {
+        return schema.invoices.all();
       });
     },
   });
